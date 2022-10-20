037b03681a4c41b13dbf06b43d44a688a99d701e
jirka <jorgetimes2@gmail.com>
2022-10-20 14:29:10 +0200

    Add route as an attribute

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,11 +5,25 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    private array $greetingsList = [
+            'Hello',
+            'Jorge',
+            'How are you'
+        ];
+
+    #[Route('/')]
     public function index(): Response
     {
         return new Response('HI!');
+    }
+
+    #[Route('/show/{id}')]
+    public function show($id): Response
+    {
+        return new Response($this->greetingsList[$id]);
     }
 }
